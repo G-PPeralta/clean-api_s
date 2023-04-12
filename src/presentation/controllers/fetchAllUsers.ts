a35a23cb12cd3342fd02194465986bb4e5e92e70
Yolanda Ferreira de Souza <landacontato@gmail.com>
2023-04-12 11:56:36 -0300

    feat: create FetchAllUserController

---
+++ src/presentation/controllers/fetchAllUsers.ts
@@ -0,0 +1,20 @@
+import { HttpResponse } from "../protocols/http";
+import { Controller } from "../protocols/controller";
+import { FetchAllUsers } from "../../domain/usecases/FetchAllUsers";
+import { serverError, ok } from "../helpers/http-helper";
+
+export class FetchAllUserController implements Controller{
+
+    constructor (private readonly fetchAllUsers: FetchAllUsers) {
+
+    }
+
+    async handle(): Promise<HttpResponse> {
+        try {
+            const users = await this.fetchAllUsers.fetch();
+            return ok(users);
+        } catch (error) {
+            return serverError();
+        }
+    }
+}
