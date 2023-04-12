3323ed3a7db1c03722ffd2f63da6d2237bb074ef
G-PPeralta <prates_peralta@hotmail.com>
2023-04-12 17:40:34 -0300

    refactor: turn back to prisma, inject dependencies

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,14 +1,19 @@
 export namespace User {
-    export interface CreateUser {
-        name: string;
-        email: string;
-        password: string;
-    }
+  export interface CreateUser {
+    name: string;
+    email: string;
+    password: string;
+  }
 
-    export interface User {
-        id: string;
-        name: string;
-        email: string;
-        password: string;
-    }
+  export interface CreateUserReturn {
+    name: string;
+    email: string;
+  }
+
+  export interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+  }
 }
--- src/domain/usecases/AddAccount.ts
+++ src/domain/usecases/AddAccount.ts
@@ -1,22 +1,23 @@
 import { User } from "../entities/user";
-import { FetchAllUsers } from "./FetchAllUsers";
 import { encodePassword } from "../../utils/encode_password";
+import { PrismaClient } from "@prisma/client";
 
 export interface AddAccountRequest {
-  add(account: User.CreateUser): Promise<User.CreateUser>;
+  add(account: User.CreateUser): Promise<User.CreateUserReturn>;
 }
 
 export class AddAccount implements AddAccountRequest {
-  constructor(private readonly fetchAllUsers: FetchAllUsers) {}
+  constructor(private prisma: PrismaClient) {}
 
   add = async (account: User.CreateUser) => {
-    const users = await this.fetchAllUsers.fetch();
-    users.push({
-      id: "5",
-      name: account.name,
-      email: account.email,
-      password: encodePassword(account.password),
+    await this.prisma.user.create({
+      data: {
+        name: account.name,
+        email: account.email,
+        password: encodePassword(account.password),
+      },
     });
-    return Promise.resolve(account);
+    const { password, ...result } = account;
+    return result;
   };
 }
