b297e7277ac5fd7bd49ad8ae235f2f66d0502cc3
G-PPeralta <prates_peralta@hotmail.com>
2023-04-12 16:05:32 -0300

    feat: implements login router

--- src/presentation/controllers/fetchAllUsers.ts
+++ src/presentation/controllers/fetchAllUsers.ts
@@ -3,18 +3,15 @@ import { Controller } from "../protocols/controller";
 import { FetchAllUsers } from "../../domain/usecases/FetchAllUsers";
 import { serverError, ok } from "../helpers/http-helper";
 
-export class FetchAllUserController implements Controller{
+export class FetchAllUserController implements Controller {
+  constructor(private readonly fetchAllUsers: FetchAllUsers) {}
 
-    constructor (private readonly fetchAllUsers: FetchAllUsers) {
-
+  async handle(): Promise<HttpResponse> {
+    try {
+      const users = await this.fetchAllUsers.fetch();
+      return ok(users);
+    } catch (error) {
+      return serverError();
     }
-
-    async handle(): Promise<HttpResponse> {
-        try {
-            const users = await this.fetchAllUsers.fetch();
-            return ok(users);
-        } catch (error) {
-            return serverError();
-        }
-    }
+  }
 }
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,40 +1,45 @@
 import { Router } from "express";
 import { FetchAllUsers } from "../domain/usecases/FetchAllUsers";
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const loginRouter = Router();
 
 const fetchAllUsers = new FetchAllUsers();
 
 loginRouter.post("/", async (req, res) => {
-    const {email, password} = req.body;
+  const { email, password } = req.body;
 
-    const users = await fetchAllUsers.fetch();
+  const users = await fetchAllUsers.fetch();
 
-    const user = users.find((u) => u.email === email);
-    
-    if(!user) {
-        return res.status(401).json({ errors: [{ msg: 'Invalid credentials' }] });
+  const user = users.find((u) => u.email === email);
+
+  if (!user) {
+    return res.status(401).json({ errors: [{ msg: "User does not exist" }] });
+  }
+
+  bcrypt.compare(
+    password,
+    user.password,
+    (err: Error | undefined, isMatch: boolean) => {
+      if (err) throw err;
+
+      if (!isMatch) {
+        console.log("isMatchDENTRODOIF", isMatch);
+        return res
+          .status(401)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const payload = { user: { id: user.id } };
+      const secret = process.env.JWT_SECRET || "meusergredo123";
+
+      jwt.sign(payload, secret, { expiresIn: "1h" }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     }
-
-   bcrypt.compare(password, user.password, (err: Error | undefined, isMatch: boolean) => {
-        if(err) throw err;
-
-        console.log(password, user.password, isMatch);
-
-        if(!isMatch) {
-            return res.status(401).json({ errors: [{ msg: 'Invalid credentials' }] });
-        }
-
-        const payload = {user: {id:user.id}}
-        const secret = process.env.JWT_SECRET || "meusergredo123";
-
-        jwt.sign(payload, secret, {expiresIn: "1h"}, (err, token) => {
-            if(err) throw err;
-            res.json({token});
-        })
-   })
-})
+  );
+});
 
 export { loginRouter };
