64b21d33c9a75abbfed1479a4894cefd4b087708
G-PPeralta <prates_peralta@hotmail.com>
2023-04-12 16:06:01 -0300

    feat: create login route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { FetchAllUsers } from "../domain/usecases/FetchAllUsers";
+import { AddAccount } from "../domain/usecases/AddAccount";
 import { FetchAllUserController } from "../presentation/controllers/fetchAllUsers";
+import { AddAccountController } from "../presentation/controllers/addAccount";
 import { serverError } from "../presentation/helpers/http-helper";
 
 const userRouter = Router();
@@ -8,13 +10,25 @@ const userRouter = Router();
 const fetchAllUsers = new FetchAllUsers();
 const fetchAllUsersController = new FetchAllUserController(fetchAllUsers);
 
+const addAccount = new AddAccount(fetchAllUsers);
+const addAccountController = new AddAccountController(addAccount);
+
 userRouter.get("/", async (req, res) => {
-    try {
-        const users = await fetchAllUsersController.handle();
-        return res.status(users.statusCode).json(users.body);
-    } catch (error) {
-        return serverError();
-    }
-})
+  try {
+    const users = await fetchAllUsersController.handle();
+    return res.status(users.statusCode).json(users.body);
+  } catch (error) {
+    return serverError();
+  }
+});
+
+userRouter.post("/", async (req, res) => {
+  try {
+    const newUser = await addAccountController.handle(req);
+    return res.status(newUser.statusCode).json(newUser);
+  } catch (error) {
+    return serverError();
+  }
+});
 
 export { userRouter };
